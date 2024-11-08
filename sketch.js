2383e5e116e54cf478884af1ae7325c4aae9822a
zzha0024 <zzha0024@uni.sydney.edu.au>
2024-11-08 11:40:40 +0800

    Iteration 4
    I add some random rectangles and more white wireframes in the fixed rectangles.

--- sketch.js
+++ sketch.js
@@ -3,8 +3,7 @@ let frames = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  //constructor NoisyFrame(x: any, y: any, w: any, h: any, baseColor: any)
-  //creating four frames with noise animation
+  // create four frames with noise animation and add to the frames array
   frames.push(new NoisyFrame(0.037 * min(width, height), 0.186 * min(width, height), 0.125 * min(width, height), 0.2 * min(width, height), color(239, 17, 17)));
   frames.push(new NoisyFrame(0.625 * min(width, height), 0.15 * min(width, height), 0.125 * min(width, height), 0.2 * min(width, height), color(43, 115, 247)));
   frames.push(new NoisyFrame(0.138 * min(width, height), 0.725 * min(width, height), 0.2 * min(width, height), 0.125 * min(width, height), color(211, 211, 211)));
@@ -15,13 +14,22 @@ function setup() {
 function draw() {
   background(255);
   drawRandomLines();
+  
   for (let frame of frames) {
-    //display each frame with noise animation
     frame.display(); 
   }
+
+  //create random rects in random position
+  randomRect(); 
+  //draw colored road at specific positions
+  drawColouredHorizontalRoad(min(width, height) / 40 * 21);
+  drawColouredVerticalRoad(min(width, height) / 40 * 1);
+  drawColouredVerticalRoad(min(width, height) / 40 * 23);
+  drawColouredHorizontalRoad(min(width, height) / 40 * 15);
+  drawColouredVerticalRoad(min(width, height) / 40 * 13);
+  drawColouredHorizontalRoad(min(width, height) / 40 * 37);
 }
 
-//draw random lines, generating a random yellow grid 
 function drawRandomLines() {
   let size = min(windowWidth, windowHeight);
   stroke(252, 224, 46);
@@ -48,7 +56,56 @@ function drawRandomLines() {
   }
 }
 
-//create a class for NoisyFrame
+//function to create random rects and avoid overlaping with fixed rects
+function randomRect() {
+  let size = min(windowWidth, windowHeight);
+  let colors = [
+    [239, 17, 17],  // red
+    [43, 115, 247], // blue
+    [211, 211, 211] // gray
+  ];
+
+  // fixed rects
+  let fixedRects = [
+    { x: 0.037 * size, y: 0.186 * size, w: 0.125 * size, h: 0.2 * size },
+    { x: 0.625 * size, y: 0.15 * size, w: 0.125 * size, h: 0.2 * size },
+    { x: 0.138 * size, y: 0.725 * size, w: 0.2 * size, h: 0.125 * size },
+    { x: 0.7 * size, y: 0.7 * size, w: 0.175 * size, h: 0.225 * size }
+  ];
+
+  for (let i = 0; i < 5; i++) {
+    let rectSize = random(20, 80);
+    let x, y;
+    let overlapping = true;
+
+    
+    while (overlapping) {
+      x = random(0, size - 70);
+      y = random(0, size - 70);
+      overlapping = false;
+ 
+      // make sure the random rects will not overlap with the fixed rects
+      for (let rect of fixedRects) {
+        let horizontalOverlap = x < rect.x + rect.w && x + rectSize > rect.x;
+        let verticalOverlap = y < rect.y + rect.h && y + rectSize > rect.y;
+
+        if (horizontalOverlap && verticalOverlap) {
+          // regenerate new position if overlapping
+          overlapping = true;
+          break;
+        }
+      }
+    }
+
+    let color = random(colors);
+    fill(color);
+    noStroke();
+    rect(x, y, rectSize, rectSize);
+  }
+}
+
+  
+//class for noisy frame
 class NoisyFrame {
   constructor(x, y, w, h, baseColor) {
     this.x = x;
@@ -56,45 +113,49 @@ class NoisyFrame {
     this.w = w;
     this.h = h;
     this.baseColor = baseColor;
-    //randomly initialize noise effect
+    //randomly initialize noise offset
     this.noiseOffset = random(10); 
   }
 
-  //function to display the frame with noise animation
   display() {
     fill(this.baseColor);
     noStroke();
     rect(this.x, this.y, this.w, this.h);
-
-    // draw wireframe in the fixed rects
+    //draw dynamic wireframe in the rectangle
     this.drawNoisyFrames();
+    //increase noise offset to create an animation effect
+    this.noiseOffset += 0.01;
   }
 
   drawNoisyFrames() {
     noFill();
-    stroke(255, 255, 255, 150); 
+    stroke(255, 255, 255, 150);
     strokeWeight(1);
+    //the number of shrinking wireframes
+    let steps = 33;
 
-    //Number of shrinking frames
-    let steps = 10; 
+    this.noiseOffset += 0.0009;
+    //use setInterval to call display() 60 times per second
+    setInterval(() => this.display(), 60);
 
     for (let i = 0; i < steps; i++) {
       let noiseValue = noise(this.noiseOffset + i * 0.1);
-      //map noise value to scaling 
-      let scale = map(noiseValue, 0, 1, 1, 0.05); 
+      let scale = map(noiseValue, 0, 1, 1, 0.05);
       let offsetX = (this.w * (1 - scale)) / 2;
       let offsetY = (this.h * (1 - scale)) / 2;
 
       rect(this.x + offsetX, this.y + offsetY, this.w * scale, this.h * scale);
     }
-
-    // increase noise offset to create an animation 
-    this.noiseOffset += 0.01;
-    //use setInterval to call display() 60 times per second
-    setInterval(() => this.display(), 60);
   }
 }
 
+//resize the canvas to fit the new window size when the window is resized
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+  frames = [
+    new NoisyFrame(0.037 * min(width, height), 0.186 * min(width, height), 0.125 * min(width, height), 0.2 * min(width, height), color(239, 17, 17)),
+    new NoisyFrame(0.625 * min(width, height), 0.15 * min(width, height), 0.125 * min(width, height), 0.2 * min(width, height), color(43, 115, 247)),
+    new NoisyFrame(0.138 * min(width, height), 0.725 * min(width, height), 0.2 * min(width, height), 0.125 * min(width, height), color(211, 211, 211)),
+    new NoisyFrame(0.7 * min(width, height), 0.7 * min(width, height), 0.175 * min(width, height), 0.225 * min(width, height), color(239, 17, 17))
+  ];
 }
